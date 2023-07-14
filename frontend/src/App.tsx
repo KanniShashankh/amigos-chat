9dbb751f3b66cabd64cb9a14048174c976907c8c
Shashankh Kanni <shashank.kanni@gmail.com>
2023-07-14 20:36:02 +0530

    conti

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,39 @@
 import  Login  from './components/Login'
-function App() {
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+// import { config } from 'dotenv';
+// config();
+// console.log(process);
+firebase.initializeApp({
+  // apiKey : process.env.REACT_APP_APIKEY,
+  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_PROJECTID,
+  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
+  // appId: process.env.REACT_APP_APPID,
+  // measurementId: process.env.REACT_APP_MEASUREMENTID,
+})
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+
+
+function App() {
+  const [user] = useAuthState(auth);
   return (
     <>
       <div className="w-screen h-screen bg-slate-900">
         <Login />
       </div>
+      {user ? "Logged in" : "Logged out"}
     </>
   )
-}
+} 
 
 export default App
