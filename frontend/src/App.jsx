99dce204662d97381682b1ca779dd27abc2a5ecf
Shashankh Kanni <shashank.kanni@gmail.com>
2023-07-15 18:52:10 +0530

    added firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,22 @@
 
 import Login from './Pages/LoginPage'
-import React, { useRef, useState } from 'react';
-import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 
-import * as firebase from 'firebase/app';
-import * as fstore from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import * as Flytics from 'firebase/analytics';
+const firebaseConfig = {
+  apiKey: "AIzaSyDBNJJ2Ey-tVRm0IPbgJO0d_S6yXcDowXc",
+  authDomain: "amigos-chat-1.firebaseapp.com",
+  projectId: "amigos-chat-1",
+  storageBucket: "amigos-chat-1.appspot.com",
+  messagingSenderId: "636112292833",
+  appId: "1:636112292833:web:6d25743b0014944fac20a0",
+  measurementId: "G-590LNKHYWX"
+};
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-const app = firebase.initializeApp({
-  //gere
-})
-
-const auth = getAuth(app);
-// const firestore = app.firestore();
-// const analytics = Flytics;
-
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
-  const [user] = useAuthState(auth);
-
   return (
     <>
       <Login />
