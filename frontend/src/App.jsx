cb06e9fe103a13f7f92898212f8d59689913875b
Shashankh Kanni <shashank.kanni@gmail.com>
2023-07-15 19:51:16 +0530

    till now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@
 import Login from './Pages/LoginPage'
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useEffect, useState } from 'react';
+import ChatRoom from './components/ChatRoom';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBNJJ2Ey-tVRm0IPbgJO0d_S6yXcDowXc",
@@ -15,11 +20,65 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+const LogOut = () => {
+  signOut(auth).then(() => {
+    localStorage.clear();
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
 
 function App() {
+  const [user] = useAuthState(auth);
+  const [userPhoto, setUserPhoto] = useState(null);
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if(user){
+      setUserPhoto(user.photoURL);
+      setName(user.displayName);
+    }
+  })
   return (
-    <>
-      <Login />
+    <>{
+      user ? 
+      <ChatRoom />
+      // <div className='bg-violet-400 h-screen w-screen'>
+      //   <p className='text-center	hover:'> Hello {name} </p>
+      //   <img className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src={userPhoto ?? "public/default.jpeg"} alt="Bordered avatar" />
+      // </div>
+      :
+      <Login loginWithGoogle={() =>{
+        signInWithPopup(auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          // const credential = GoogleAuthProvider.credentialFromResult(result);
+          // const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          localStorage.setItem('user', JSON.stringify(user));
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        }).catch((error) => {
+          console.error(error);
+
+          // Handle Errors here.
+          // const errorCode = error.code;
+          // const errorMessage = error.message;
+          // // The email of the user's account used.
+          // const email = error.customData.email;
+          // // The AuthCredential type that was used.
+          // const credential = GoogleAuthProvider.credentialFromError(error);
+          // ...
+        });
+      }} /> 
+    }
+      
     </>
   )
 }
--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Login = () => {
+const Login = (props) => {
   return (
     <div className="w-screen h-screen bg-slate-900 flex justify-center items-center ">
       <div className="w-fit h-fit  bg-white p-10 rounded-lg" >
       <div className="flex items-center justify-center">
-        <button className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+        <button onClick={()=>props.loginWithGoogle()} className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
           <svg
             className="h-6 w-6 mr-2"
             xmlns="http://www.w3.org/2000/svg"
